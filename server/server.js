c21509c96779ba38f6847150df7a42bd30b05f4f
David Komolafe <davekomo24@gmail.com>
2023-12-16 16:59:21 +0100

    first commit

---
+++ server/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(Express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+const PORT = process.env.PORT || 5000;

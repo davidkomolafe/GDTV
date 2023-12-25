9c02422237f39e7016afda1a98f8312bd7bf321c
David Komolafe <davekomo24@gmail.com>
2023-12-25 19:40:19 +0100

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignUp from "./Screens/SignUp/SignUp";
+import Login from "./Screens/Login/Login";
+import Layout from "./Layout/Layout";
+import Catalog from "./Screens/Catalog/Catalog";
 
 function App() {
   return (
-    <div>
-      <SignUp />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/*" element={<Layout />} />
+        <Route path="/catalog" element={<Catalog />} />
+      </Routes>
+    </Router>
   );
 }
 
